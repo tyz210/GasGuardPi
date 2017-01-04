b54163c5be20bfc16edc1f207cd2221b47f7104e
Volodymyr Surgai <volodymyr.surgai@gmail.com>
2017-01-04 17:03:52 +0200

    Add UI

---
+++ main.py
@@ -0,0 +1,24 @@
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.boxlayout import BoxLayout
+
+from kivy.config import Config
+
+Config.set('graphics', 'width', '400')
+Config.set('graphics', 'height', '240')
+
+
+class MainView(BoxLayout):
+    pass
+
+
+class GasGuardApp(App):
+    def build(self):
+        Window.clearcolor = (0.1176, 0.0392, 0.1176, 1)
+        self.title = 'GasGuard'
+        return MainView()
+
+
+if __name__ == '__main__':
+    app = GasGuardApp()
+    app.run()
