9acfcfc219e34a9f3a796ace861cc742f6471e21
Volodymyr Surgai <volodymyr.surgai@gmail.com>
2017-01-04 17:39:50 +0200

    Move main view kv file into res

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
 
 from kivy.config import Config
 
 Config.set('graphics', 'width', '400')
 Config.set('graphics', 'height', '240')
+
+Builder.load_file('res/ui/gas_guard.kv')
 
 
 class MainView(BoxLayout):
