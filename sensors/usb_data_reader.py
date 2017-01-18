673d55aef6efcef0be9e6d183a2123a200008cf6
Volodymyr Surgai <volodymyr.surgai@gmail.com>
2017-01-19 00:31:32 +0200

    [sensor] Add exception handling in usb_data_reader.py

--- sensors/usb_data_reader.py
+++ sensors/usb_data_reader.py
@@ -1,4 +1,26 @@
 import serial
-ser = serial.Serial('/dev/cu.usbmodemFD121', 9600)
-while True:
-    print ser.readline()
+
+SERIAL_PORT = 9600
+
+# TODO: Add device detection
+ser = serial.Serial('/dev/cu.usbmodemFD121', SERIAL_PORT)
+
+
+def publish_data(data):
+    # TODO: Add more output sources
+    print(data)
+
+
+def main():
+    ser.close()
+    ser.open()
+    try:
+        while True:
+            # TODO: Add timer for data reading
+            publish_data(data=ser.readline())
+    except:
+        ser.close()
+        print('\nClose read session.')
+
+if __name__ == "__main__":
+    main()
