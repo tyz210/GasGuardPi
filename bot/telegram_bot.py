b1c0ab2039f11c32c9f936304e54847da62efb36
Volodymyr Surgai <volodymyr.surgai@gmail.com>
2017-02-22 23:07:10 +0200

    :construction: Partial bot implementation

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -4,18 +4,38 @@
 import telebot
 import keyring
 
-from credentials import BOT_KEY_NAMESPACE, BOT_TOKEN
+from telebot import types
+
+from bot.credentials import BOT_KEY_NAMESPACE, BOT_TOKEN
 
 TOKEN = keyring.get_password(BOT_KEY_NAMESPACE, BOT_TOKEN)
 
 bot = telebot.TeleBot(TOKEN)
 
+LOCATION_KEYS = ['home', 'paren_home']
+
+
+def test():
+    print('sdfsdf')
+
+
+@bot.message_handler(commands=['start', 'help'])
+def handle_start_help(message):
+    markup = types.ReplyKeyboardRemove()
+    markup.selective(test)
+
+    bot.send_message(message.chat.id, 'Choose:', reply_markup=markup)
+
+
+@bot.message_handler(commands=LOCATION_KEYS)
+def handle_status_help(message):
+    bot.send_message(message.chat.id, 'Status: {}'.format('data'))
+
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-
-
-    bot.send_message(message.chat.id, 'You say: {}'.format(message.text))
+    if message.text in LOCATION_KEYS:
+        bot.send_message(message.chat.id, 'Status at {}: {}'.format(message.text, '{"data": 42}'))
 
 
 if __name__ == '__main__':
